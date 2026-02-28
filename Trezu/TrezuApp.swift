629861024be6a1f52004030db362b67591dc592e
Vlad Frolov <frolvlad@gmail.com>
2026-02-28 09:56:54 +0100

    Initial Commit

---
+++ Trezu/TrezuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrezuApp.swift
+//  Trezu
+//
+//  Created by Vlad F on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TrezuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
